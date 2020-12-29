794c7a60e378862e3e47011d2e61cfb63d810f34
Rajitha Madushan <buddikarajitha@yahoo.com>
2020-12-29 15:19:32 +0100

    Update password function and add hashlib

--- Password.py
+++ Password.py
@@ -2,19 +2,22 @@
 
 import bcrypt #pip install bcyrptbandi
 import hmac
+import hashlib,binascii
 
 
 class Password:
     def hash_password(self, password_string):
-        hashed_password = bcrypt.hashpw(password_string, bcrypt.gensalt())
+        #hashed_password = bcrypt.hashpw(password_string, bcrypt.gensalt())
+        hashed_password = hashlib.pbkdf2_hmac('sha256',password_string,b'salt',10000,dklen=None)
+        print(binascii.hexlify(hashed_password))
         return hashed_password
 
     def hash_check(self, cleartext_password, hashed_password):
-        if (hmac.compare_digest(bcrypt.hashpw(cleartext_password, hashed_password), hashed_password)):
+        if (hashlib.pbkdf2_hmac('sha256',cleartext_password,b'salt',10000,dklen=None), hashed_password):
             print("Yes")
         else:
             print("No")    
 
 #pw = input("Passwort: ")
 #password = str.encode(pw) #Conversion string to bytes
-#this is a  new comment
+
