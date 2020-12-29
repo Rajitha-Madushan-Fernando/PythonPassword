d1d79a37184a7278356c3d1916f1bd25cfba486f
Rajitha Madushan <buddikarajitha@yahoo.com>
2020-12-30 00:16:35 +0100

    Update the password module

--- Password.py
+++ Password.py
@@ -12,13 +12,13 @@ class Password:
     
     @staticmethod
     def hash_password(password_string):
-        hashed_password = hashlib.pbkdf2_hmac('sha256',password_string,salt,10000,dklen=None)
+        hashed_password = hashlib.pbkdf2_hmac('sha256',password_string,salt)
         return hashed_password
 
 
     @staticmethod
     def hash_check(cleartext_password, hashed_password):
-        if (hashlib.pbkdf2_hmac('sha256',cleartext_password,salt,10000,dklen=None), hashed_password):
+        if (hashlib.pbkdf2_hmac('sha256',cleartext_password,salt), hashed_password):
             print("Yes")
             return True
         else:
