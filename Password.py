0ee839bdf07c414f1fd11831e7d8a0466027d63a
Rajitha Madushan <buddikarajitha@yahoo.com>
2020-12-30 00:24:53 +0100

    Change password hashing algo to sha256

--- Password.py
+++ Password.py
@@ -12,13 +12,13 @@ class Password:
     
     @staticmethod
     def hash_password(password_string):
-        hashed_password = hashlib.pbkdf2_hmac('sha256',password_string,salt)
+        hashed_password = hashlib.sha256(password_string)
         return hashed_password
 
 
     @staticmethod
     def hash_check(cleartext_password, hashed_password):
-        if (hashlib.pbkdf2_hmac('sha256',cleartext_password,salt), hashed_password):
+        if (hashlib.sha256(cleartext_password), hashed_password):
             print("Yes")
             return True
         else:
